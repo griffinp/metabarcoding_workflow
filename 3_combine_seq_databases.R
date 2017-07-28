a88f3d96c3a95cbfb196d7c9f244628a085411f6
Pip Griffin <pip.griffin@gmail.com>
2017-07-28 12:31:14 +1000

    Fix bug that was causing some files to be omitted

--- 3_combine_seq_databases.R
+++ 3_combine_seq_databases.R
@@ -8,11 +8,11 @@
 # not sure if this will be a problem for dada2?
 # no, doesn't appear to cause any problems
 
-base_dir <- "~/Documents/Research_Projects/metabarcoding/metaR/ref_files/"
+base_dir <- "~/Documents/Research_Projects/metabarcoding/metaR/ref_files"
 search_string_1 <- paste(base_dir, "/taxon_list1/*dada2*", sep="")
 search_string_2 <- paste(base_dir, "/taxon_list2/*dada2*", sep="")
 search_string_3 <- paste(base_dir, "/private_ref_sequences/*dada2*", sep="")
-search_string <- c(search_string_1, search_string_2, search_string_3)
+search_string <- paste(c(search_string_1, search_string_2, search_string_3), collapse=" ")
 dada2_matches <- system(command=paste('ls', search_string), intern=TRUE)
 collapsed <- paste(dada2_matches, collapse=" ")
 system(command=paste("cat", collapsed, "> ~/Documents/Research_Projects/metabarcoding/metaR/ref_files/dada2_ref_db_2017-07-25.fasta"))
